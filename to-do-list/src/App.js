edb1ffcea6f04e1779ca2acbf0e56e19dd01ebdd
Jungmin Moon <jm.moon0801@gmail.com>
2023-08-14 23:03:48 +0900

    20230814 simple to-do-list

---
+++ to-do-list/src/App.js
@@ -0,0 +1,38 @@
+
+import { useState } from "react";
+
+function App() {
+  const [todo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(todo === '') {
+      return ;
+    }
+    setToDos((currentArray) => [todo, ...currentArray]);
+    setToDo("");
+  }
+  // console.log(toDos)
+
+
+  return (
+    <div className="App">
+      <h1>My To Dos ({toDos.length}) </h1>
+      <form onSubmit={ onSubmit }>
+        <input 
+        onChange= { onChange }
+        value={ todo }
+        type="text" 
+        placeholder="Write your to do...."/>
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      {
+        toDos.map((item,i) => <li key={i}>{item}</li> )
+      }
+    </div>
+  );
+}
+
+export default App;
